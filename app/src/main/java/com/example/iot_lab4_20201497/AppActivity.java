e412a3b79f2208f2de7749c5da9afdd4a3c00f05
Yosthim <a20201497@pucp.edu.pe>
2024-10-05 23:31:04 -0500

    Avance pregunta 1 - App activity

--- app/src/main/java/com/example/iot_lab4_20201497/AppActivity.java
+++ app/src/main/java/com/example/iot_lab4_20201497/AppActivity.java
@@ -7,6 +7,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class AppActivity extends AppCompatActivity {
 
@@ -20,5 +26,33 @@ public class AppActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        BottomNavigationView navView = findViewById(R.id.bottom_navigation);
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.leaguesFragment, R.id.positionsFragment, R.id.resultsFragment)
+                .build();
+
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(navView, navController);
+
+        // Remover los fragmentos del back stack (hecho con ayuda de claude)
+        navController.addOnDestinationChangedListener((controller, destination, arguments) -> {
+            if (destination.getId() == R.id.leaguesFragment ||
+                    destination.getId() == R.id.positionsFragment ||
+                    destination.getId() == R.id.resultsFragment) {
+                controller.popBackStack(controller.getGraph().getStartDestination(), false);
+            }
+        });
+
     }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        return NavigationUI.navigateUp(navController, (AppBarConfiguration) null)
+                || super.onSupportNavigateUp();
+    }
+
 }
